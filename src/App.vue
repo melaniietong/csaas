15fdd8c26c00b155f149a1736b944c74055f385d
Melanie Tong <melaniietong@gmail.com>
2026-02-13 21:28:18 -0500

    move meta description to app for content injection

--- src/App.vue
+++ src/App.vue
@@ -16,7 +16,13 @@ const goToGithub = (): void => {
   )
 }
 
+// Metadata
 document.title = content.title
+
+const metaDesc = document.querySelector('meta[name="description"]')
+if (metaDesc) {
+  metaDesc.setAttribute('content', content.tagline)
+}
 </script>
 
 <template>
